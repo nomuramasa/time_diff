1b8de2cd26719329e772a6568bc5625cd8e927c0
nommy <nomuramasam@gmail.com>
2020-02-19 13:02:25 +0900

    目盛りに値を表示できない

--- vue.js
+++ vue.js
@@ -6,10 +6,24 @@ new Vue( {
   },
 	methods: {
 		move: function(){ // レンジが動いたとき
-			this.time_a = Number(this.time_a) // レンジinputの値を数値に直す
-			this.time_b = Number(this.time_b)
+			// 数値型にする
+			this.time_a = Number(this.time_a);  this.time_b = Number(this.time_b) // レンジinputの値を数値に直す
+
+			// 時差を算出
 			this.time_b = this.time_a - 2 // ベトナム時間を、日本時間より2時間遅れにする
-			console.log(this.time_b, this.time_a) 
+
+			// ループ
+			if(this.time_a > 24){ // 25時になったら
+				this.time_a = this.time_a - 24 // 1時にする
+			}else if(this.time_a < 1){ // 0時になったら
+				this.time_a = this.time_a + 24 // 24時にする
+			}
+			// bも
+			if(this.time_b > 24){ // 25時になったら
+				this.time_b = this.time_b - 24 // 1時にする
+			}else if(this.time_b < 1){ // 0時になったら
+				this.time_b = this.time_b + 24 // 24時にする
+			}
 		},
 	},
 } )
