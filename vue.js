1b0ed2f71be2e243cca0b06fe39744ce6beced81
nomuramasa <nomuramasam@gmail.com>
2020-02-20 14:04:00 +0900

    進むボタン。連打できない

--- vue.js
+++ vue.js
@@ -1,8 +1,15 @@
 new Vue( {
   el: '#app',
   data: {
-    time_a: 9, // 日本時間
-    time_b: 16 // バンクーバー時間@カナダ
+    month_a: 2, // 日本月
+    date_a: 20, // 日本日付
+    day_a: '木', // 日本曜日
+		time_a: 9, // 日本時間
+
+    month_b: 2, // カナダ月
+    date_b: 20, // カナダ日付
+    day_b: '木', // カナダ曜日
+    time_b: 16 // カナダ時間
   },
 	methods: {
 		move: function(x){ // レンジが動いたとき
@@ -18,7 +25,23 @@ new Vue( {
 			}
 			
 
-			// ループ
+			// 左右ループ
+			if(this.time_a > 23){ // 25時になったら
+				this.time_a = this.time_a - 24 // 1時にする
+			}else if(this.time_a < 0){ // 0時になったら
+				this.time_a = this.time_a + 24 // 24時にする
+			}
+			// bも
+			if(this.time_b > 23){ // 25時になったら
+				this.time_b = this.time_b - 24 // 1時にする
+			}else if(this.time_b < 0){ // 0時になったら
+				this.time_b = this.time_b + 24 // 24時にする
+			}
+		},
+		forw: function(){ // 進む
+			this.time_a = this.time_a + 1
+			this.time_b = this.time_b + 1
+			// 左右ループ
 			if(this.time_a > 23){ // 25時になったら
 				this.time_a = this.time_a - 24 // 1時にする
 			}else if(this.time_a < 0){ // 0時になったら
